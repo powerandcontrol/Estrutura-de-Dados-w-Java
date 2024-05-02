c31b51badd99ffbe438adf3892ce2fcc5fcbab87
pe <141767130+powerandcontrol@users.noreply.github.com>
2024-05-01 21:37:49 -0300

    Delete Projetos/Atendimento Preferencial/bin/Paciente.java

--- Projetos/Atendimento Preferencial/bin/Paciente.java
+++
@@ -1,12 +0,0 @@
-Êþº¾   = 
-       java/lang/Object <init> ()V	  	  
- 
-  Paciente nome Ljava/lang/String;	     
-prioritario Z (Ljava/lang/String;Z)V Code LineNumberTable 
-ehPrioritario ()Z  getNome ()Ljava/lang/String; 
-SourceFile 
-Paciente.java !       
-                 3     *· *+µ  *µ 
-±              	                 *´ 
-¬           
-             *´  °                 
